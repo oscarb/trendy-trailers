7830f22b5fa8b83059533505917dd1fc13be1197
oscarb <git@oscarb.se>
2017-01-18 22:41:34 +0100

    Create basis for MVVM packaged by feature

--- app/src/main/java/se/oscarb/trendytrailers/explore/ExploreActivity.java
+++ app/src/main/java/se/oscarb/trendytrailers/explore/ExploreActivity.java
@@ -1,15 +1,17 @@
-package se.oscarb.trendytrailers;
+package se.oscarb.trendytrailers.explore;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import se.oscarb.trendytrailers.R;
+
+public class ExploreActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

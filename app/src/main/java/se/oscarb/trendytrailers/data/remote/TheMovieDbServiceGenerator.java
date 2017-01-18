edb0690c46cb81f382ef53f2ab7f9d5ef37a138a
oscarb <git@oscarb.se>
2017-01-18 23:47:30 +0100

    Prepare and provide API service

--- app/src/main/java/se/oscarb/trendytrailers/data/remote/TheMovieDbServiceGenerator.java
+++ app/src/main/java/se/oscarb/trendytrailers/data/remote/TheMovieDbServiceGenerator.java
@@ -1,5 +1,8 @@
 package se.oscarb.trendytrailers.data.remote;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
 public class TheMovieDbServiceGenerator {
 
     public static final String API_BASE_URL = "https://api.themoviedb.org/3/";
@@ -7,5 +10,16 @@ public class TheMovieDbServiceGenerator {
 
     private static TheMovieDbService service;
 
+    private static Retrofit.Builder builder = new Retrofit.Builder()
+            .baseUrl(API_BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create());
+
+    public static TheMovieDbService getService() {
+        if (service == null) {
+            Retrofit retrofit = builder.build();
+            service = retrofit.create(TheMovieDbService.class);
+        }
+        return service;
+    }
 
 }

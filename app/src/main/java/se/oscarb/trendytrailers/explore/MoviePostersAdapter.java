1897f78ee0d428933bf7c0d8b80460ba7d1aa8b8
oscarb <git@oscarb.se>
2017-01-20 00:17:02 +0100

    Create and bind viewholder

--- app/src/main/java/se/oscarb/trendytrailers/explore/MoviePostersAdapter.java
+++ app/src/main/java/se/oscarb/trendytrailers/explore/MoviePostersAdapter.java
@@ -1,12 +1,15 @@
 package se.oscarb.trendytrailers.explore;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import java.util.Collections;
 import java.util.List;
 
+import se.oscarb.trendytrailers.R;
+import se.oscarb.trendytrailers.databinding.ItemPosterBinding;
 import se.oscarb.trendytrailers.model.Movie;
 
 public class MoviePostersAdapter extends RecyclerView.Adapter<MoviePostersAdapter.ViewHolder> {
@@ -31,23 +34,32 @@ public class MoviePostersAdapter extends RecyclerView.Adapter<MoviePostersAdapte
 
     @Override
     public MoviePostersAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        ItemPosterBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.item_poster, parent, false);
+        return new ViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(MoviePostersAdapter.ViewHolder holder, int position) {
-
+        holder.bindMovie(movies.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return movies.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final ItemPosterBinding binding;
 
-        public ViewHolder(View itemView) {
-            super(itemView);
+
+        public ViewHolder(ItemPosterBinding itemViewBinding) {
+            super(itemViewBinding.getRoot());
+            binding = itemViewBinding;
         }
+
+        private void bindMovie(Movie movie) {
+
+        }
+
     }
 }

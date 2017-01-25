3723169edf9981bd58106ae91f95418f38ec8f65
oscarb <git@oscarb.se>
2017-01-25 02:29:32 +0100

    Show rating, release date and plot

--- app/src/main/java/se/oscarb/trendytrailers/detail/DetailViewModel.java
+++ app/src/main/java/se/oscarb/trendytrailers/detail/DetailViewModel.java
@@ -1,5 +1,11 @@
 package se.oscarb.trendytrailers.detail;
 
+import android.databinding.BindingAdapter;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import se.oscarb.trendytrailers.data.remote.TheMovieDbServiceGenerator;
 import se.oscarb.trendytrailers.model.Movie;
 import se.oscarb.trendytrailers.model.ViewModel;
 
@@ -8,13 +14,44 @@ public class DetailViewModel implements ViewModel {
 
     private Movie movie;
 
+    public DetailViewModel() {
+        // movie = new Movie();
+    }
+
     public DetailViewModel(Movie movie) {
         this.movie = movie;
     }
 
+    @BindingAdapter("imageUrl")
+    public static void setImageUrl(ImageView imageView, String url) {
+        Glide.with(imageView.getContext())
+                .load(url)
+                .into(imageView);
+    }
 
     public String getTitle() {
         return movie.getOriginalTitle();
+    }
+
+    public String getPosterUrl() {
+        return TheMovieDbServiceGenerator.IMAGE_BASE_URL + "w185" + movie.getPosterPath();
+    }
+
+    public String getReleaseDate() {
+        return movie.getReleaseDate();
+    }
+
+    public float getRating() {
+        return movie.getVoteAverage() / 2;
+    }
+
+    public String getPlotSynopsis() {
+        return movie.getOverview();
+    }
+
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
     }
 
     @Override
